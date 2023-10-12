5aa8b8f8480f39de9944d6e33ed253deb46ea824
stijn <stijn.hawinkel@psb.ugent.be>
2023-10-12 16:56:33 +0200

    building the read in method

--- R/buildHyperFrame.R
+++ R/buildHyperFrame.R
@@ -1,2 +1,13 @@
 #' Built the hyperframe containing all separate point patterns that will
 #'  be used throughout the analysis. Both matrices, dataframe and SpatialExperiment inputs are accepted.
+#' @rdname buildHyperFrame
+#' @export
+setGeneric("buildHyperFrame", function(x, ...) standardGeneric("buildHyperFrame"))
+#' @rdname buildHyperFrame
+#' @export
+setMethod("buildHyperFrame", "data.frame", function(x, ...) {
+})
+#' @rdname buildHyperFrame
+#' @export
+setMethod("buildHyperFrame", "SpatialExperiment", function(x, ...) {
+})
