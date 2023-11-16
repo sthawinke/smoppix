2f78788112e28587a013a2a5e92fb83050dcf0c8
stijn <stijn.hawinkel@psb.ugent.be>
2023-11-16 16:53:59 +0100

    warning for yang

--- R/calcWindowDistPI.R
+++ R/calcWindowDistPI.R
@@ -8,9 +8,11 @@
 #' @importFrom matrixStats rowMins
 #' @return The estimated probabilistic index
 #' @importFrom spatstat.geom nncross centroid.owin edges
-#' @details Analysis of the distance to the border was introduced by \insertCite{Joyner2013} in the form of the B-function.
+#' @details Analysis of the distance to the border was introduced by \insertCite{Joyner2013}{spatrans} in the form of the B-function.
 #' The independent evaluations of the B-functions per cell are here returned as realizations of the probabilistic index.
 #' @seealso [addCell]
+#' @references
+#' \insertAllCited{}
 calcWindowDistPI = function(pSub, owins, ecdfAll, towhat){
     splitPPP = split.ppp(pSub, f = "cell")
     obsDistEdge = lapply(names(splitPPP), function(x){
