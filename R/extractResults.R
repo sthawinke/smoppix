be0e22a5c1695852cc9bf6c4a5427e54641b0548
stijn <stijn.hawinkel@psb.ugent.be>
2023-11-27 17:37:44 +0100

    Function to extract results

---
+++ R/extractResults.R
@@ -0,0 +1,8 @@
+extractResults = function(models, fixedVars){
+    out = lapply(Vars <- c("Intercept", fixedVars), function(Var){
+
+        lapply(models, function(x){
+           })
+    })
+    names(out) = Vars
+}
