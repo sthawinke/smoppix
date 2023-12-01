12292609d3b8e31d53f04678d8789195008278af
stijn <stijn.hawinkel@psb.ugent.be>
2023-12-01 16:53:01 +0100

    Separate calcPIuni function

--- R/estPims.R
+++ R/estPims.R
@@ -97,34 +97,8 @@ estPimsSingle = function(p, pis, null, tabObs, nSims = 5e1, nPointsAll = 2e3,
     }
     if(verbose)
         message("Calculating univariate probabilistic indices...")
-    uniPIs = lapply(nams <- names(tabObs[features])[!idOne], function(feat){
-        pSub = p[id <-which(marks(p, drop = FALSE)$gene == feat), ]
-        p = p[-id, ] #Avoid zero distances by removing observations of gene
-        NNdistPI = if(any(pis == "nn")){
-            calcNNPI(pSub, p, null, nSims, ecdfs = ecdfs[id], n = nSub)}
-        #Also here room for improvement
-        allDistPI = if(any(pis == "allDist")){
-            calcAllDistPI(pSub, p, ecdfAll = ecdfAll, null = null,
-                          ecdfs = ecdfs[id])}
-        edgeDistPI = if(any(pis == "edge")){
-            calcWindowDistPI(pSub, owins, ecdfAll = ecdfsEdgeAndMidpoint,
-                             towhat = "edge")}
-        midPointDistPI = if(any(pis == "midpoint")){
-            calcWindowDistPI(pSub, owins, ecdfAll = ecdfsEdgeAndMidpoint,
-                             towhat = "midpoint")}
-        nnCellPI = if(any(pis == "nnCell")){
-            calcWindowDistPI(pSub, owins, ecdfAll = ecdfsEdgeAndMidpoint,
-                             towhat = "nn")}
-        allDistCellPI = if(any(pis == "allDistCell")){
-            calcWindowDistPI(pSub, owins, ecdfAll = ecdfsEdgeAndMidpoint,
-                             towhat = "allDist")}
-        fixedPointDistPI = if(any(pis == "fixedpoint")){
-            calcFixedPointDistPI(pSub, pointPPP, ecdfAll = ecdfFixedPoint)}
-        list("pointDists" = c("nn" = NNdistPI, "allDist" = allDistPI),
-             "windowDists" = list("edge" = edgeDistPI, "nnCell" = nnCellPI,
-                    "allDistCell" = allDistCellPI, "midpoint" = midPointDistPI,
-                                  "fixedpoint" = fixedPointDistPI))
-    }); names(uniPIs) = nams
+    uniPIs = calcUniPIs(p, pis, ecdfsEdgeAndMidpoint, owins, tabObs, null,
+                        nSims, ecdfs, nSub, ecdfAll, idOne)
     #Bivariate patterns
     biPIs = if(any(piPair <- grepl(pis, pattern = "Pair"))){
         featuresBi = features[tabObs[features] > 0]
