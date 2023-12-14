521af3f6afa1bc3266f957ad951f88188b8f2dee
stijn <stijn.hawinkel@psb.ugent.be>
2023-12-14 17:00:28 +0100

    Limit number of points plotted

--- R/plotExplore.R
+++ R/plotExplore.R
@@ -2,7 +2,9 @@
 #'
 #' @param hypFrame The hyperframa
 #' @param features A small number of features to be fitted. Defaults to the first 5
-#' @param ppps The rownames or indices of the point patterns to be plotted. Defaults to the first 9.
+#' @param ppps The rownames or indices of the point patterns to be plotted.
+#' Defaults to the first 8.
+#' @param maxPlot The maximum number of events plotted per point pattern
 #'
 #' @return Plots a facet of point patterns to output
 #' @importFrom spatstat.geom is.hyperframe
@@ -17,26 +19,32 @@
 #'     imageVars = c("day", "root", "section")
 #' )
 #' plotExplore(hypYang)
-plotExplore <- function(hypFrame, features = attr(hypFrame, "features"), ppps) {
-    stopifnot(is.hyperframe(hypFrame), is.character(features))
+plotExplore <- function(hypFrame, features = attr(hypFrame, "features"), ppps,
+                        maxPlot = 2e5) {
+    stopifnot(is.hyperframe(hypFrame), is.character(features),
+              is.numeric(maxPlot))
     npp <- nrow(hypFrame)
     if (missing(ppps)) {
-        ppps <- rownames(hypFrame)[seq_len(min(8, npp))]
+        ppps <- seq_len(min(8, npp))
     }
     Cols <- palette()
     Cols <- c(Cols, rep("grey", length(features) - length(Cols)))
     names(Cols) <- features
     old.par <- par(no.readonly = TRUE)
     on.exit(par(old.par))
-    par(mfrow = if ((LL <- length(ppps)) <= 3) c(2, 2) else c(3, 3), mar = c(3, 3, 3, 3))
+    par(mfrow = if ((LL <- length(ppps)) <= 3) c(2, 2) else c(3, 3),
+        mar = c(3, 3, 3, 3))
     baa <- lapply(ppps, function(i) {
         id <- marks(hypFrame$ppp[[i]], drop = FALSE)$gene %in% features
         if (any(id)) {
+            if(length(id) > maxPlot)
+                id = sample(id, maxPlot)
             colVec <- Cols[marks(hypFrame$ppp[[i]], drop = FALSE)$gene[id]]
             cordMat <- coords(hypFrame$ppp[[i]][id, ])
-            ordVec <- order(colVec != "grey") # Plot grey background first and coloured dots on top
+            ordVec <- order(colVec != "grey")
+            # Plot grey background first and coloured dots on top
             plot(cordMat[ordVec, ],
-                main = ppps[i], pch = ".", asp = 1,
+                main = hypFrame$image[ppps[i]], pch = ".", asp = 1,
                 col = colVec[ordVec]
             )
         }
