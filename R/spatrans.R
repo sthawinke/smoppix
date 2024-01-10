7b66e07866af5091c28da160d7b9d3d6fa655a2f
stijn <stijn.hawinkel@psb.ugent.be>
2024-01-10 14:31:05 +0100

    working on package docu

--- R/spatrans.R
+++ R/spatrans.R
@@ -1,3 +1,5 @@
 #' @useDynLib spatrans, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
+#' @docType package
+#' @name spatrans
 #'Analyze Spatial Transcriptomics and Other Spatial Omics Data on the Single Molecule Scale
