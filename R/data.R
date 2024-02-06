f9a2936cb4345a4be6e9cfeb0c3c3d7e7f937155
stijn <stijn.hawinkel@psb.ugent.be>
2024-02-06 14:35:54 +0100

    With eng data

--- R/data.R
+++ R/data.R
@@ -15,3 +15,19 @@
 #' \insertAllCited{}
 #' @usage data(Yang)
 "Yang"
+#' Spatial transcriptomics data of mouse fibroblast cells
+#'
+#' Single-molecule spatial transcriptomics seqFISH+ data containing measurements of 10,000 genes in NIH/3T3 mouse fibroblast cells by
+#' \insertCite{Eng2019}{spatrans}. Molecule locations, gene identity and design
+#' variables are included. In addition, a list of regions of interest (rois) is given describing the cell boundaries
+#' @format A data matrix
+#' \describe{
+#'   \item{x,y}{Molecule coordinates}
+#'   \item{gene}{Character vector with gene identities}
+#'  \item{experiment,fov}{Design variables}
+#' }
+#' @source \doi{10.1016/j.cub.2023.08.030}
+#' @references
+#' \insertAllCited{}
+#' @usage data(Eng)
+"Eng"
