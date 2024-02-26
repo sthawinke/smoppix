b963ee1249524a8c9804ac450052b411d8adc5d4
stijn <stijn.hawinkel@psb.ugent.be>
2024-02-26 16:21:20 +0100

    Updated docu

--- R/buildMoransIWeightMat.R
+++ R/buildMoransIWeightMat.R
@@ -1,21 +1,20 @@
 #' Build a weight matrix for Moran's I calculations
-#' @param coordList A list of coordinate matrices
+#' @param coordMat A matrix of centroid coordinates
 #' @inheritParams fitLMMs
 #' @return A sparse matrix of weights for Moran's I statistic, of equal value
 #' for the numNNs nearest neighbours and zero otherwise
 #' @importFrom Matrix sparseMatrix
 #' @importFrom spatstat.geom nnwhich
-buildMoransIWeightMat <- function(coordList, numNNs) {
-    numNNs <- min(numNNs, length(coordList) - 3)
+buildMoransIWeightMat <- function(coordMat, numNNs) {
+    numNNs <- min(numNNs, nrow(coordMat) - 3)
     if (numNNs < 0) {
-        matrix(0, nrow(coordList), nrow(coordList))
+        matrix(0, nrow(coordMat), nrow(coordMat))
     }
     # Maximum as many NN as there are neighbouring cells
-    coordMat <- t(vapply(coordList, FUN.VALUE = double(2), getCoordsMat))
-    nns <- nnwhich(X = coordMat[, 1], Y = coordMat[, 2], k = seq.int(numNNs))
+    nns <- nnwhich(X = coordMat[, "x"], Y = coordMat[, "y"], k = seq.int(numNNs))
     # Assign non-zero slots to all numNNs nearest neighbours
     sparseMatrix(i = rep(seq_len(nrow(coordMat)), numNNs), j = nns, x = 1/numNNs,
-        symmetric = FALSE, dimnames = list(names(coordList), names(coordList)))
+        symmetric = FALSE, dimnames = list(rownames(coordMat), rownames(coordMat)))
     # Sparse, but not symmetric, as nearest neighbours are not always
     # reciprocal
 }
