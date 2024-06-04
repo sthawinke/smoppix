4634ff1ea3a0e4226890a76fe1a2ccec2ee77b7a
stijn <stijn.hawinkel@psb.ugent.be>
2024-06-04 12:57:04 +0200

    bugfix

--- R/plotCells.R
+++ R/plotCells.R
@@ -129,7 +129,7 @@ plotCells <- function(
     addLegend(Cols, shiftVec(counter, Ceils[1]))
     if (colourBorder) {
         borderCols <- unique(unlist(borderCols))
-        names(borderCols)[seq_along(unVals)] <- unVals
+        names(borderCols) <- unVals[[seq_along(borderCols)]]
         addLegend(borderCols, shiftVec(counter + 1, Ceils[1]),
             Pch = 5, Main = borderColVar,
             Cex = 0.7
