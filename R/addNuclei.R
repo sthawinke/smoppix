1407ee755886eb7795b55edaffe7d2877bd9b27b
stijn <stijn.hawinkel@psb.ugent.be>
2024-08-28 11:41:51 +0200

    Add plotting nuclei

--- R/addNuclei.R
+++ R/addNuclei.R
@@ -3,40 +3,20 @@
 #' check in which cell each event lies and add a cell marker.
 #'
 #' @param hypFrame The hyperframe
-#' @param owins the list containing a list of owins per point pattern.
+#' @param nucleiList the list containing a list of owins per point pattern.
 #' The length of the list must match the length of the hyperframe, and the names must match.
-#' Also lists of geojson objects, coordinate matrices or rois are accepted, see details.
-#' @param cellTypes A dataframe of cell types and other cell-associated covariates.
-#' If supplied, it must contain a variable 'cell' that is matched with the names of the owins
-#' @param findOverlappingOwins a boolean, should windows be checked for overlap?
-#' @param warnOut a boolean, should warning be issued when points are not
-#' contained in window?
+#' Also lists of geojson objects, coordinate matrices or rois are accepted, see \link{addCell}
 #' @param coords The names of the coordinates, if the windows are given as sets of coordinates.
 #' @param verbose A boolean, should verbose output be printed?
-#' @param addCellMarkers A boolean, should cell identities be added? Set this to
-#'  FALSE if cell identifiers are already present in the data, and you only want
-#'  to add windows and centroids.
+#' @param checkSubset A boolean, should be checked whether nuclei are encompassed by cells?
 #' @param ... Further arguments passed onto \link{convertToOwins}
-#' @return The hyperframe with cell labels added in the marks of the point patterns
+#'
+#' @return The hyperframe with nuclei added as entry
 #' @importFrom spatstat.geom is.subset.owin
 #' @export
-#' @seealso \link{buildHyperFrame}, \link{convertToOwins}
-#' @details First the different cells are checked for overlap per point pattern.
-#' If no overlap is found, each event is assigned the cell that it falls into.
-#' Events not belonging to any cell will trigger a warning and be assigned 'NA'.
-#' Cell types and other variables are added to the marks if applicable.
-#' This function employs multithreading through the BiocParallel package for
-#' converting windows to owins as well as for finding overlap between the windows
-#' in the findOverlap() function when checkOverlap is TRUE.
-#' If this leads to excessive memory usage and crashes, try serial processing by
-#' setting register(SerialParam()).
-#' Different formats of windows are allowed, if the corresponding packages are installed.
-#' A dataframe of coordinates or a list of spatstat.geom owins is always allowed, as necessary packages are required by smoppix.
-#' A 'SpatialPolygonsDataFrame' object is allowed if the polycub package is installed,
-#' and a list of 'ijroi' object or a single 'ijzip' object if the 'RImageJROI' package is installed.
-#' @note By default, overlap between windows is not checked.
-#' Events are assigned to the first window they fall in. If you are not sure of the quality of the segmentation,
-#' do check your input or set checkOverlap to TRUE, even when this make take time.
+#' @seealso \link{addCell}, \link{convertToOwins}
+#' @details The nuclei names must match the cell names already present, all other nuclei are dropped. 
+#' A warning is issued when nuclei are not encompassed by their cell
 #' @examples
 #' library(spatstat.random)
 #' set.seed(54321)
@@ -75,15 +55,16 @@
 #' names(wList) <- rownames(hypFrame) # Matching names is necessary
 #' hypFrame2 <- addCell(hypFrame, wList)
 #' # The nuclei
-#' n1 <- owin(poly = list(x = c(0.1, .4, 0.9, .4), y = c(.4, .1, .4, .9)))
+#' n1 <- owin(poly = list(x = c(0.2, .4, 0.8, .4), y = c(.4, .2, .4, .8)))
 #' n2 <- owin(poly = list(x = c(0.1, 0.1, .4), y = c(.4, .1, .1)))
-#' n3 <- owin(poly = list(x = c(0.1, 0.1, .4), y = c(.9, .4, .9)))
-#' n4 <- owin(poly = list(x = c(.9, .9, .4), y = c(.4, .9, .9)))
-#' n5 <- owin(poly = list(x = c(.9, .9, .4), y = c(.1, .4, .1)))
+#' n3 <- owin(poly = list(x = c(0.1, 0.1, .4), y = c(1, .75, 1)))
+#' n4 <- owin(poly = list(x = c(1, 1, .6), y = c(.7, .9, .9)))
+#' n5 <- owin(poly = list(x = c(.95, .95, .7), y = c(.1, .4, .1)))
 #' nList <- lapply(seq_len(nDesignFactors), function(x) {
 #'     list("w1" = n1, "w2" = n2, "w3" = n3, "w4" = n4, "w5" = n5)
 #' })
-#' hypFrame2 <- addNuclei(hypFrame2, nList)
+#' names(nList) <- rownames(hypFrame) # Matching names is necessary
+#' hypFrame3 <- addNuclei(hypFrame2, nList)
 addNuclei <- function(hypFrame, nucleiList, checkSubset = TRUE,
                     verbose = TRUE, coords = c("x", "y"), ...) {
     stopifnot(
